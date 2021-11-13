591af4216991a4930ac9b78cb9a3088b75f9fa50
Piter <81367231+Szczerbaniak@users.noreply.github.com>
2021-11-13 18:54:29 +0100

    Add files via upload

---
+++ ToDoList/scripts.js
@@ -0,0 +1,83 @@
+let tasks;
+
+function addTask() {
+    const newTask = document.getElementById('taskInput').value;
+    let cookies = document.cookie;
+    let newCookieNum;
+    let correctInput = false;
+    let incorrecttInput = ["task", ":", ";"];
+
+    document.getElementById('taskInput').value = "";
+
+    for(i = 0; i <incorrecttInput.length; i++) {
+        if(newTask.toLowerCase().includes(incorrecttInput[i])) {
+            correctInput = false;
+            window.alert(`Zadanie nie może zawierać ${incorrecttInput[i]}`);
+        }else{
+            correctInput = true;
+        }
+    }
+    
+
+    if(correctInput == true) {
+        if(isNaN(cookies[cookies.lastIndexOf('task') + 4] * 1)) {
+            newCookieNum = 0;
+        }else{
+            newCookieNum = ((cookies.substr(cookies.lastIndexOf('task') + 4, (cookies.lastIndexOf('=')) - (cookies.lastIndexOf('task') + 4))) * 1) + 1;
+            console.log('test' + ((cookies.substr(cookies.lastIndexOf('task') + 4, (cookies.lastIndexOf('=')) - (cookies.lastIndexOf('task') + 4))) * 1) + 1);
+        }
+
+        console.log(newCookieNum);
+    
+        document.cookie = `task${newCookieNum}=${newTask}%^&*; path=/;`;
+        cookies = document.cookie;
+    
+        console.log(cookies);
+    
+        taskListMaker();
+    }
+}
+
+function deleteTask(currentTask) {
+    let cookies = document.cookie;
+
+    document.cookie = `${currentTask}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    console.log(`${currentTask}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`);
+    cookies = document.cookie;
+    console.log(cookies);
+
+    taskListMaker();
+
+}
+
+
+function taskListMaker() {
+    let cookies = document.cookie;
+    let taskList = "";
+    console.log(cookies);
+
+    taskMaker();
+
+    console.log(tasks);
+
+        for(i = 0; i < tasks.length; i++) {
+            taskList += `<div class="taskContainer">
+            <p class="task">${tasks[i].substr(tasks[i].indexOf('=') + 1)}</p> 
+            <button class="button" onclick="deleteTask('task${tasks[i].substr(0, (tasks[i].indexOf('=')))}')">x</button>
+            </div>`;
+        }
+    document.getElementById("tasks").innerHTML = taskList;
+}
+
+function taskMaker() {
+    let cookies = document.cookie;
+
+    tasks = cookies.split('%^&*');
+
+    for(i = 0; i < tasks.length; i++) {
+        tasks[i] = tasks[i].replace('task', '');
+        tasks[i] = tasks[i].replace('; ', '');
+    }
+
+    tasks.pop();
+}
